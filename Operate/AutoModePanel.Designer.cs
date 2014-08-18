7f0c4c923b46dc9484e0d14e3bc92e972ac764f9
damonlin <damonalbarn1351@hotmail.com>
2014-08-18 10:04:29 +0800

    修改 Project 命名

--- Operate/AutoModePanel.Designer.cs
+++ Operate/AutoModePanel.Designer.cs
@@ -148,7 +148,7 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.BackgroundImage = global::Operate.Properties.Resources.圖控頁面1;
+            this.pictureBox1.BackgroundImage = global::AutoMode.Properties.Resources.圖控頁面1;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(-1, 117);
             this.pictureBox1.Name = "pictureBox1";
